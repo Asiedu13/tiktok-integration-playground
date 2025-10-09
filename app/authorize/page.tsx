fa851d4d2cfe462e6c32e1e8209c476914130e8e
Prince Asiedu <princekofasiedu@gmail.com>
2025-10-09 04:37:08 +0000

    fix incorrect url params type

--- app/authorize/page.tsx
+++ app/authorize/page.tsx
@@ -2,16 +2,29 @@ import {Log} from "@/app/utils/functions";
 import {redirect} from "next/navigation";
 
 interface searchParamsType {
-    [item: string]: string,
+    searchParams: { [key: string]: string | string[] | undefined };
 }
 
 export default function Page({searchParams}: searchParamsType) {
     console.log({searchParams})
-    const authorizedURLParams: string =  new URLSearchParams(searchParams).toString();
-    const newRedirectURI: string = `http://localhost:3000/?${authorizedURLParams}`;
+    const authorizedURLParams: URLSearchParams =  new URLSearchParams();
 
+
+    for (const key in searchParams) {
+        const value = searchParams[key];
+
+        if (typeof value === "string") {
+            authorizedURLParams.append(key,  String(value));
+
+        } else if (Array.isArray(value)) {
+            value.forEach(val => {
+                    authorizedURLParams.append(key,  String(val))
+            })
+        }
+    }
+
+    const newRedirectURI: string = `http://localhost:3000/?${authorizedURLParams.toString()}`;
     console.log(newRedirectURI);
-
     redirect(newRedirectURI);
 
     return (
